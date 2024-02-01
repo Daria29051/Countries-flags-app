b0f012366d178218d45058f837784b9f0d4da4c5
unknown <daria.lantyugova@mail.ru>
2024-02-01 12:40:42 +0300

    redux toolkit createSlice and asyncThunk added 2nd version

---
+++ src/features/controls/use-region.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRegion, selectRegion } from './controls-slice';
+
+export const useRegion = () => {
+    const dispatch = useDispatch();
+    const region = useSelector(selectRegion);
+    const handleSelect = (reg)=> {
+      dispatch(setRegion(reg?.value || ''))
+    }
+
+  return [region, handleSelect];
+}
